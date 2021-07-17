0c9400a9bb1d401d00af0bf26a8c4594f772064b
clark1015 <seok626898@gmail.com>
2021-07-17 16:41:11 +0900

    time error resolution

--- 과제/1주차/3079입국심사.py
+++ 과제/1주차/3079입국심사.py
@@ -1,14 +1,14 @@
+import sys
+
 def count(time: int):
     cnt:int=0
     for i in range(n):
         cnt+=time//T[i]
 
     return cnt
-T = []    
-n,m=map(int, input().split())
-for p in range(n):
-    k=int(input())
-    T.append(k)
+
+n,m=map(int, sys.stdin.readline().split())
+T = [int(sys.stdin.readline()) for p in range(n)]    
 
 lt: int=1
 rt: int=max(T)*m
